4c724ff43821cc6dc3e01b8bc0f02dd0853cbe79
Carsten Lund <clu@rts.dk>
2025-01-27 12:14:44 +0100

    updated index.js not to use DOMContentLoaded

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+// slå dig løs her... 
 
-    // slå dig løs her... 
-
-    //eksempel på at udskrive alle overskrifter i services i konsollen:
-    services.forEach(service => console.log(service.headline))
-
-}) // DOMContentLoaded slut
+//eksempel på at udskrive alle overskrifter i services i konsollen:
+services.forEach(service => console.log(service.headline))
